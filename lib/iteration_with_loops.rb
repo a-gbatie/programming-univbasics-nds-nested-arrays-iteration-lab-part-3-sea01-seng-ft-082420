2d8d62c8d3ed93040a1f6ded14f1f7b3b3f0ed2e
Ange Batie <ange.batie@gmail.com>
2020-08-15 02:43:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,9 @@
 def join_nested_strings(src)
-  
+  empty_string = ""
+  row_index = 0 
+  while row_index < src.length do
+    element_index = 0 
+    while element_index < src[row_index].length do
+      if src[row_index][element_index].class == String
+        empty_string 
 end
